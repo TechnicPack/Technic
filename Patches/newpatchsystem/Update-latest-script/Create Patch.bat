4a50c52eb265d51416b01bdc01e463a1299da16e
timvrakas <tim.vrakas@gmail.com>
2012-10-10 16:12:22 -0500

    added newpatchsystem

---
+++ Patches/newpatchsystem/Update-latest-script/Create Patch.bat
@@ -0,0 +1,12 @@
+@ECHO OFF
+title Technic Patch Maker
+
+:new
+set /P newversion=What is the new version of Minecraft: %=%
+if "%newversion%"=="" goto new
+
+
+java -classpath diff.jar ie.wombat.jbdiff.JBDiff minecraft_%newversion%.jar 1.0.jar latest.patch
+echo Done
+pause
+Exit
